56047e3a8deee238279d0fabe4cd4e53aadbe6e6
hanybakeer <hany.bakeer@gmail.com>
2026-03-29 23:02:15 +0200

    almost done normalizer

--- ngram-predictor/src/data_prep/normalizer.py
+++ ngram-predictor/src/data_prep/normalizer.py
@@ -1,8 +1,10 @@
 # add main() if __name__=="__main__"
 
 import os
+from pydoc import text
 import re
 import string
+#from dotenv import load_dotenv
 
 
 class Normalizer:
@@ -15,9 +17,7 @@ class Normalizer:
     Returns:
     tokenized file
 
-    """
-    
-    
+    """    
     def load(self,folder_path: str) -> str:
         """
         Load all .txt files in a folder.
@@ -50,27 +50,54 @@ class Normalizer:
         # 1) lowercase
         text = text.lower()
 
-        # 2) remove numbers
-        text = re.sub(r'\d+', '', text)
-
-        # 3) remove punctuation, but keep . ! ?
+        # 2) remove punctuation, but keep . ! ?
         punctuation_to_remove = string.punctuation.replace('.', '').replace('!', '').replace('?', '')
         text = text.translate(str.maketrans('', '', punctuation_to_remove))
-
+        
+        # 3) remove numbers
+        text = re.sub(r'\d+', '', text)
+        
         # 4)    remove extra whitespace
-        text = re.sub(r'\s+', ' ', text).strip()
+        text = re.sub(r'[ \t]+', ' ', text)
 
         # 5) remove spaces before sentence punctuation
         text = re.sub(r'\s+([.!?])', r'\1', text)
 
         return text
-
+    def sentence_tokenize(self,text: str) -> list[str]:
+        sentences = re.split(r'(?<=[.!?])\s+', text.strip())
+        return [s for s in sentences if s]
+    def word_tokenize(self,sentences: list[str]) -> list[str]:
+        return [word for sentence in sentences for word in sentence.split()]
+    def write_list_to_file(self,items_sentence: list[str],items_word: list[str], output_file: str)->None:
+        """
+        This method write list to file
+        Parameters: text (str)
+        Returns: file written
+        """
+        with open(output_file, "w", encoding="utf-8") as f:
+            for item_sentence in items_sentence:
+                f.write(item_sentence + "\n")
+            for item_word in items_word:
+                f.write(item_word + "")
+        return text   
 #tests
-#hany = Normalizer().load(r"C:\AI\python\practice\ngram_rep\ngram-predictor\data\raw\train")
-hany = Normalizer().load(r"D:\AI\Python\practice\ngram_capstone\ngram_rep\ngram-predictor\data\raw\train\test")
+
+TRAIN_RAW_DIR="D:\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\raw\\train"
+EVAL_RAW_DIR="  D:\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\raw\eval"
+TRAIN_TOKENS="D:\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\processed\\train_tokens.txt"
+EVAL_TOKENS="D:\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\processed\\eval_tokens.txt"
+MODEL="D:\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\model\model.json"
+VOCAB="D:\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\model\vocab.json"
+UNK_THRESHOLD=3
+TOP_K=3
+NGRAM_ORDER=4
+hany = Normalizer().load(TRAIN_RAW_DIR)
 hany2 = Normalizer().strip_gutenberg(hany)
 hany3 = Normalizer().normalizer(hany2)
-
-print(hany3)
+hany4a = Normalizer().sentence_tokenize(hany3)
+hany4b = Normalizer().word_tokenize(hany4a)
+hany5 = Normalizer().write_list_to_file(hany4a,hany4b,TRAIN_TOKENS)
+#print(hany4b)
 
 
