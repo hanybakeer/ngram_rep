9a201415190c8db6e1e7bdeb8ba2de80ca085976
hanybakeer <hany.bakeer@gmail.com>
2026-03-29 21:17:13 +0200

    Update normalizer.py

--- ngram-predictor/src/data_prep/normalizer.py
+++ ngram-predictor/src/data_prep/normalizer.py
@@ -1,11 +1,8 @@
 # add main() if __name__=="__main__"
 
-# from ast import Load
-#from email.mime import text
 import os
 import re
-#from pickle import load
-#from tracemalloc import start
+import string
 
 
 class Normalizer:
@@ -33,9 +30,7 @@ class Normalizer:
                 file_path = os.path.join(folder_path, filename)
                 with open(file_path, "r", encoding="utf-8") as f:
                     text += f.read() + "\n"
-        return text
-    
-
+        return text 
     def strip_gutenberg(self,in_text:str)->str:
         """
         This method removes all headers and footers from the text
@@ -46,23 +41,36 @@ class Normalizer:
         r"\*\*\* START OF THE PROJECT GUTENBERG EBOOK.*?\*\*\*\s*(.*?)\s*\*\*\* END OF THE PROJECT GUTENBERG EBOOK.*?\*\*\*",re.DOTALL)
         books = pattern.findall(in_text)
         return "\n\n".join(book.strip() for book in books)
-        
-        #text = ""
-        #header_pattern = "*** START OF THE PROJECT GUTENBERG EBOOK"
-        #footer_pattern = "*** END OF THE PROJECT GUTENBERG EBOOK"
-        #header_index = in_text.find(header_pattern)
-        #footer_index = in_text.find(footer_pattern)
-        #print(header_index) 
-        #print(footer_index)
-        #part = in_text[header_index:footer_index]
-        #part = in_text[3:4]
+    def normalizer(self,text:str)->str:
+        """
+        This method applies lowercase, remove punctuation, remove numbers, and remove extra whitespace in order.
+        Parameters: text (str)
+        Returns: str normalized
+        """
+        # 1) lowercase
+        text = text.lower()
 
+        # 2) remove numbers
+        text = re.sub(r'\d+', '', text)
+
+        # 3) remove punctuation, but keep . ! ?
+        punctuation_to_remove = string.punctuation.replace('.', '').replace('!', '').replace('?', '')
+        text = text.translate(str.maketrans('', '', punctuation_to_remove))
+
+        # 4)    remove extra whitespace
+        text = re.sub(r'\s+', ' ', text).strip()
+
+        # 5) remove spaces before sentence punctuation
+        text = re.sub(r'\s+([.!?])', r'\1', text)
+
+        return text
 
 #tests
 #hany = Normalizer().load(r"C:\AI\python\practice\ngram_rep\ngram-predictor\data\raw\train")
-hany = Normalizer().load(r"D:\AI\Python\practice\ngram_capstone\ngram_rep\ngram-predictor\data\raw\train")
+hany = Normalizer().load(r"D:\AI\Python\practice\ngram_capstone\ngram_rep\ngram-predictor\data\raw\train\test")
 hany2 = Normalizer().strip_gutenberg(hany)
+hany3 = Normalizer().normalizer(hany2)
 
-print(hany2)
+print(hany3)
 
 
