7b4fc8ea650c3e350235c94187e31fe534322d7d
selimabdelrahman <saleem.abdelrahman@gmail.com>
2026-04-15 18:00:35 +0200

    global vars removal

--- src/data_prep/normalizer.py
+++ src/data_prep/normalizer.py
@@ -84,47 +84,48 @@ class Normalizer:
         return text   
 #tests
 
-"""
-#works for hany
-TRAIN_RAW_DIR="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\raw\\train"
-EVAL_RAW_DIR="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\raw\eval"
-TRAIN_TOKENS="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\processed\\train_tokens.txt"
-EVAL_TOKENS="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\processed\\eval_tokens.txt"
-MODEL="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\model\model.json"
-VOCAB="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\model\vocab.json"
-#Testing branch
-"""
+if __name__=="__main__":
+    """
+    #works for hany
+    TRAIN_RAW_DIR="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\raw\\train"
+    EVAL_RAW_DIR="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\raw\eval"
+    TRAIN_TOKENS="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\processed\\train_tokens.txt"
+    EVAL_TOKENS="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\processed\\eval_tokens.txt"
+    MODEL="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\model\model.json"
+    VOCAB="D:\\AI\Python\practice\\ngram_capstone\\ngram_rep\\ngram-predictor\data\\model\vocab.json"
+    #Testing branch
+    """
 
 
-"""
-#works for salma
-TRAIN_RAW_DIR= "c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\raw\\train"
-EVAL_RAW_DIR="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\raw\\eval"
-TRAIN_TOKENS="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\processed\\train_tokens.txt"
-EVAL_TOKENS="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\processed\\eval_tokens.txt"
-MODEL="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\model\\model.json"
-VOCAB="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\model\\vocab.json"
-"""
+    """
+    #works for salma
+    TRAIN_RAW_DIR= "c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\raw\\train"
+    EVAL_RAW_DIR="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\raw\\eval"
+    TRAIN_TOKENS="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\processed\\train_tokens.txt"
+    EVAL_TOKENS="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\processed\\eval_tokens.txt"
+    MODEL="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\model\\model.json"
+    VOCAB="c:\\AI\\python\\practice\\ngram_rep\\ngram-predictor\\data\\model\\vocab.json"
+    """
 
-#"""
-#works for selim
-TRAIN_RAW_DIR="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\raw\\train"
-EVAL_RAW_DIR="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\raw\\eval"
-TRAIN_TOKENS="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\processed\\train_tokens.txt"
-EVAL_TOKENS="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\processed\\eval_tokens.txt"
-MODEL="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\model\model.json"
-VOCAB="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\model\vocab.json"
-#Testing branch
-#"""
+    #"""
+    #works for selim
+    TRAIN_RAW_DIR="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\raw\\train"
+    EVAL_RAW_DIR="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\raw\\eval"
+    TRAIN_TOKENS="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\processed\\train_tokens.txt"
+    EVAL_TOKENS="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\processed\\eval_tokens.txt"
+    MODEL="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\model\model.json"
+    VOCAB="C:\\Users\\sabdelra\\N_Gram\\ngram_rep\\ngram-predictor\data\\model\vocab.json"
+    #Testing branch
+    #"""
 
-UNK_THRESHOLD=3
-TOP_K=3
-NGRAM_ORDER=4
-hany = Normalizer().load(TRAIN_RAW_DIR)
-hany2 = Normalizer().strip_gutenberg(hany)
-hany3 = Normalizer().normalizer(hany2)
-hany4a = Normalizer().sentence_tokenize(hany3)
-##hany4b = Normalizer().word_tokenize(hany4a)
-hany5 = Normalizer().write_list_to_file(hany4a,'',TRAIN_TOKENS)
-#print(hany4b)
+    UNK_THRESHOLD=3
+    TOP_K=3
+    NGRAM_ORDER=4
+    hany = Normalizer().load(TRAIN_RAW_DIR)
+    hany2 = Normalizer().strip_gutenberg(hany)
+    hany3 = Normalizer().normalizer(hany2)
+    hany4a = Normalizer().sentence_tokenize(hany3)
+    ##hany4b = Normalizer().word_tokenize(hany4a)
+    hany5 = Normalizer().write_list_to_file(hany4a,'',TRAIN_TOKENS)
+    #print(hany4b)
 
